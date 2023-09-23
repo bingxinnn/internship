cd4bf6067c9fab1900b1329190c9253a4e4d2fd2
bingxinnn <143489588+bingxinnn@users.noreply.github.com>
2023-09-23 18:46:38 +0800

    Delete js directory

--- js/main2.js
+++
@@ -1,9 +0,0 @@
-/*===== LOGIN SHOW and HIDDEN =====*/
-// Get a reference to the button element
-const button = document.getElementById('login-in');
-
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-    // Redirect to another HTML page
-        window.location.href = 'home.html';
-    });
